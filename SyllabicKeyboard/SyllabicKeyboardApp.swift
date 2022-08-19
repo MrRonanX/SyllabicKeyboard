a45e5cce70d5e3e96b031cb054aad067b396660e
Roman Kavisnkyi <roman_kavinskyi@pecodesoftware.com>
2022-08-19 15:31:53 +0700

    Initial Commit

---
+++ SyllabicKeyboard/SyllabicKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyllabicKeyboardApp.swift
+//  SyllabicKeyboard
+//
+//  Created by Roman Kavinskyi on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct SyllabicKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
